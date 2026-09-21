54dd5d0af068d05004bb0d67b6a50c6909521809
Vova <vovakondysiuk20@gmail.com>
2026-09-21 12:05:07 +0700

    fix after review #2 (remove @GeneratedValue)

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Table;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String email;
